45fb92187bd43a53661bf683145c6d39e2b247f4
adir-avraham <adir.avraham@gmail.com>
2019-11-01 20:55:31 +0200

    get-cars

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
-
-
 const api = express()
+const port = 2000;
 
-api.listen(2000)
+const cars = require('./cars.json')
+
+
+api.listen(port, (err)=>{
+    if (err) console.log(err.message);
+    console.log(`API is listening to port: ${port}`);
+})
+
+
+api.get('/cars', (req, res, next)=> {
+
+    return res.send(`Array of cars: ${JSON.stringify(cars)}`)
+})
